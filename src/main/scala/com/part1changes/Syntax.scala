bfc98b7f8c03a7b16969fcaca139d0cea13596bf
Sergio Cano <canosergio90@gmail.com>
2023-04-05 23:06:42 +0200

    adding scalafmt plugin
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/com/part1changes/Syntax.scala
+++ src/main/scala/com/part1changes/Syntax.scala
@@ -2,58 +2,58 @@ package com.part1changes
 
 object Syntax:
 
-    // personal note: scala 2 code is compatible with scala 3
+  // personal note: scala 2 code is compatible with scala 3
 
-    // Scala 2 style
-    val ifExpression = if (2 > 3) "bigger" else "smaller"
+  // Scala 2 style
+  val ifExpression = if (2 > 3) "bigger" else "smaller"
 
-    // Scala 3
-    val ifExpression_v2 = if 2 > 3 then "bigger" else "smaller"
+  // Scala 3
+  val ifExpression_v2 = if 2 > 3 then "bigger" else "smaller"
 
-    val ifExpression_v3 =
-        if (2 > 3) {
-            val result = "code"
-            // code
-            result
-        } else {
-            val result = "other result"
-            // code
-            result
-        }
-
-    // Scala 3: braceless
-    val ifExpression_v4 =
-        if 2 > 3 then
-            val result = "code"
-            // code
-            result
-        else
-            val result = "smaller"
-            // code
-            result
-        // identation matters
-
-    val whileExpression: Unit = while (2 > 3) {
-        println("some sentence")
-        println("another sentence")
+  val ifExpression_v3 =
+    if (2 > 3) {
+      val result = "code"
+      // code
+      result
+    } else {
+      val result = "other result"
+      // code
+      result
     }
 
-    val whileExpression_v2: Unit = while 2 > 3 do
-        println("some expression")
-        println("another expression")
-        // identation matters
+  // Scala 3: braceless
+  val ifExpression_v4 =
+    if 2 > 3 then
+      val result = "code"
+      // code
+      result
+    else
+      val result = "smaller"
+      // code
+      result
+    // identation matters
 
-    // for-comprehension
-    val forComprehension =
-        for {
-            num <- List(1,2,3)
-            char <- List('a', 'b')
-        } yield s"$num-$char"
+  val whileExpression: Unit = while (2 > 3) {
+    println("some sentence")
+    println("another sentence")
+  }
 
-    val forComprehension_v2 =
-        for
-            num <- List(1,2,3)
-            char <- List('a', 'b')
-        yield s"$num-$char"
+  val whileExpression_v2: Unit = while 2 > 3 do
+    println("some expression")
+    println("another expression")
+    // identation matters
 
-    def main(args: Array[String]): Unit = {}
+  // for-comprehension
+  val forComprehension =
+    for {
+      num <- List(1, 2, 3)
+      char <- List('a', 'b')
+    } yield s"$num-$char"
+
+  val forComprehension_v2 =
+    for
+      num <- List(1, 2, 3)
+      char <- List('a', 'b')
+    yield s"$num-$char"
+
+  def main(args: Array[String]): Unit = {}
